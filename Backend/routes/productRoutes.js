8a1e5323c5b1ccec26cf73f7b84a45823fcdfa67
jayashriravikumar <rkjshri2021@gmail.com>
2026-03-05 14:16:13 +0530

    CRUD operations

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { getAllProducts, getSingleProduct } from '../controller/productcontroller.js';
+import { addProducts,getAllProducts, getSingleProduct,updateProduct,deleteProduct } from '../controller/productcontroller.js';
 const router = express.Router();
 
-router.get("/products",getAllProducts);
-router.get("/product",getSingleProduct);
-
+router.route("/products").get(getAllProducts).post(addProducts);
+//   router.get("/product/:id",getSingleProduct);
+router.route("/product/:id").get(getSingleProduct).put(updateProduct).delete(deleteProduct);
 export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
 
 dotenv.config({path:'backend/config/config.env'});
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
