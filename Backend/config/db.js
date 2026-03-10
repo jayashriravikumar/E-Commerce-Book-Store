a0518f50b8688150d1cc88c6994344768ae238a0
Sukumar.B <sukumar200508@gmail.com>
2026-03-10 11:45:04 +0530

    connected with the backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+<<<<<<< HEAD
 export const connectDB = () => {
   mongoose
     .connect(process.env.DB_URI, {
@@ -10,3 +11,15 @@ export const connectDB = () => {
       console.log("MongoDB connected with server: " + data.connection.host);
     });
 };
+=======
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+>>>>>>> 8cfcf95 (connected with the backend)
