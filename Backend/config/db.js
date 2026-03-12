0a0b25b5a103293b9b669c5149985265e1fe1b9c
Akshhhh777 <sriakshaya1610@gmail.com>
2026-03-12 10:47:40 +0530

    added seeder.js , ordermodel.js and usermodel.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
-import mongoose from "mongoose";
+
 export const connectDB = () => {
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL, {
+    serverSelectionTimeoutMS: 5000,
+    family: 4
+})
 .then((data)=>{
     console.log("MongoDB connected with server: "+data.connection.host);
 })
 .catch((err)=>{
     console.error(err.message);
 });
-
 };
--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
 
-dotenv.config({path:'backend/config/config.env'});
+dotenv.config({path:'./config/config.env'});
 const PORT = process.env.PORT || 3000;
 
 connectDB();
