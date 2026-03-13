88091e0faa30ab21f24af3ef8f56dbfa85c4c812
Vaisaki <vaisakimagesh@gmail.com>
2026-03-13 15:29:51 +0530

    password encoding

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import product from './routes/productRoutes.js';
-import errorHandler from './middleware/error.js';
+import express from "express";
+import product from "./routes/productRoutes.js";
+import user from "./routes/userRoutes.js";
+import errorHandler from "./middleware/error.js";
 
 const app = express();
-app.use(express.json());
-app.use('/api/v1',product);
-app.use(errorHandler);
 
+app.use(express.json());
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+
+app.use(errorHandler);
 
 export default app;
