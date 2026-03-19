5cee0db17398651e69c3c758c5818da681a39f8e
jayashriravikumar <rkjshri2021@gmail.com>
2026-03-19 18:29:14 +0530

    login functionality with token generation

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,9 +1,20 @@
 import User from "../models/userModel.js";
-
+import { sendToken } from "../helper/jwtToken.js";
+import HandleError from "../helper/handleError.js";
 // Register User
 export const registerUser = async (req, res, next) => {
-  try {
     const { name, email, password } = req.body;
+
+    if(!name){
+      return next(new HandleError("Please enter your name", 400));  
+    }
+    if(!email){
+      return next(new HandleError("Please enter your email", 400));  
+    }
+    if(!password){
+      return next(new HandleError("Please enter your password", 400));  
+    }
+
     const user = await User.create({
       name,
       email,
@@ -13,59 +24,24 @@ export const registerUser = async (req, res, next) => {
         url: "temp_url",
       },
     });
-
-    const token = user.getJWTToken();
-
-    res.status(201).json({
-      success: true,
-      token,
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  
+    sendToken(user, 201, res);
 };
 
 // Login User
 export const loginUser = async (req, res, next) => {
-  try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Please enter email and password",
-      });
+      return next(new HandleError("Email or password cannot be empty ", 400));
     }
 
     const user = await User.findOne({ email }).select("+password");
 
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid email or password",
-      });
+    const isValidPassword = await user.verifyPassword(password);
+    if (!isValidPassword) {
+      return next(new HandleError("Invalid email or password", 401));
     }
-
-    const isPasswordMatched = await user.comparePassword(password);
-
-    if (!isPasswordMatched) {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid email or password",
-      });
-    }
-
-    const token = user.getJWTToken();
-
-    res.status(200).json({
-      success: true,
-      token,
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  
+    sendToken(user, 200, res);
 };
