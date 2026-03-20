9bb386264a8b42e99e47f8c2738b68ae6b7d55c2
jayashriravikumar <rkjshri2021@gmail.com>
2026-03-20 08:22:16 +0530

    jwt verfication

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { addProducts,getAllProducts, getSingleProduct,updateProduct,deleteProduct } from '../controller/productcontroller.js';
+import { verifyUser } from '../helper/userAuth.js';
 const router = express.Router();
 
-router.route("/products").get(getAllProducts).post(addProducts);
+router.route("/products").get(verifyUser, getAllProducts).post(addProducts);
 //   router.get("/product/:id",getSingleProduct);
 router.route("/product/:id").get(getSingleProduct).put(updateProduct).delete(deleteProduct);
 export default router;
