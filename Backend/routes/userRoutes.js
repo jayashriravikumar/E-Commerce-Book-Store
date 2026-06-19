5a7ed5c3151eba434d41f8f60301c957c9f5cb22
jayashriravikumar <rkjshri2021@gmail.com>
2026-06-19 09:19:24 +0530

    Rate limiting integrated

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,23 @@
 import express from "express";
 import { profile,registerUser,loginUser,logout,forgetPassword,resetPassword,updatePassword ,updateProfile,getUsers,getSingleUser, updateUserRole, deleteUser,verifyEmailOTP} from "../controller/userController.js";
 import { verifyUser,roleBasedAccess} from "../helper/userAuth.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+const authLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    max: 5, // Start blocking after 5 requests
+    message: { 
+        success: false, 
+        message: "Too many login attempts from this IP, please try again after an hour." 
+    },
+});
+
+router.route("/register").post(authLimiter,registerUser);
+router.route("/login").post(authLimiter,loginUser);
 router.route("/logout").get(logout);
-router.route("/password/forget").post(forgetPassword);
+router.route("/password/forget").post(authLimiter,forgetPassword);
 router.route("/reset/:token").post(resetPassword);
 router.route("/profile").get(verifyUser,profile);
 router.route("/password/update").put(verifyUser,updatePassword);
