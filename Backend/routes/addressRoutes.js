6fa0741bf62b0ba2ffc3d3ba82e31865ea239825
gera24ai-collab <sriakshaya1610@gmail.com>
2026-07-22 10:35:17 +0530

    Fix address route authentication

--- Backend/routes/addressRoutes.js
+++ Backend/routes/addressRoutes.js
@@ -7,24 +7,10 @@ import {
   deleteAddress,
 } from "../controller/addressController.js";
 
+import { isAuthenticatedUser } from "../middleware/auth.js"; // use your real auth middleware
+
 const router = express.Router();
 
-// Temporary auth middleware
-const isAuthenticatedUser = (req, res, next) => {
-  req.user = {
-    _id: "685a00000000000000000001",
-  };
-
-  next();
-};
-router.route("/address/new").post(addAddress);
-
-router.route("/addresses/me").get(getMyAddresses);
-
-router
-  .route("/address/:id")
-  .put(updateAddress)
-  .delete(deleteAddress);
 router.post(
   "/address/new",
   isAuthenticatedUser,
