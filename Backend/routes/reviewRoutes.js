25830946bc8d924c2b59743bab31c9f416d55dd2
Sukumar.B <sukumar200508@gmail.com>
2026-07-22 10:29:27 +0530

    add  review security

--- Backend/routes/reviewRoutes.js
+++ Backend/routes/reviewRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { getAllReviews } from "../controller/reviewController.js";
+import { verifyUser, roleBasedAccess } from "../helper/userAuth.js";
 
 const router = express.Router();
 
-// GET all reviews
-router.get("/admin/reviews", getAllReviews);
+router.get(
+  "/admin/reviews",
+  verifyUser,
+  roleBasedAccess("admin"),
+  getAllReviews
+);
 
 export default router;
